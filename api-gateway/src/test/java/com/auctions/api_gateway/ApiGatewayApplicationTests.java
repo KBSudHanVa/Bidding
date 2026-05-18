90bf165cdd5a23989d35dffdc33756ec7807c806
KbSudhanva9 <bhargava_konakalla@tecnics.com>
2026-05-18 17:12:25 +0530

    api-gateway

---
+++ api-gateway/src/test/java/com/auctions/api_gateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.auctions.api_gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
