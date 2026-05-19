bf964b1d5d813d52cac3a64f1a606de6c015f9bb
KbSudhanva9 <bhargava_konakalla@tecnics.com>
2026-05-19 13:43:01 +0530

    fixed the gateway filter

--- api-gateway/src/main/java/com/auctions/api_gateway/filter/JwtAuthenticationFilter.java
+++ api-gateway/src/main/java/com/auctions/api_gateway/filter/JwtAuthenticationFilter.java
@@ -1,23 +1,29 @@
 package com.auctions.api_gateway.filter;
 
-import org.springframework.cloud.gateway.filter.GatewayFilterChain;
-import org.springframework.http.HttpStatus;
-import org.springframework.stereotype.Component;
-import org.springframework.web.server.ServerWebExchange;
-
 import com.auctions.api_gateway.util.JwtUtil;
-
 import io.jsonwebtoken.Claims;
 import lombok.RequiredArgsConstructor;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.core.Ordered;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
 
 @Component
 @RequiredArgsConstructor
-public class JwtAuthenticationFilter {
+public class JwtAuthenticationFilter
+        implements GlobalFilter, Ordered {
 
     private final JwtUtil jwtUtil;
 
-    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+    @Override
+    public Mono<Void> filter(
+            ServerWebExchange exchange,
+            GatewayFilterChain chain
+    ) {
 
         String path = exchange.getRequest()
                 .getURI()
@@ -30,7 +36,7 @@ public class JwtAuthenticationFilter {
                 path.contains("/auth/refresh")
         ) {
 
-            return exchange.getResponse().setComplete();
+            return chain.filter(exchange);
         }
 
         String authHeader = exchange.getRequest()
@@ -64,15 +70,23 @@ public class JwtAuthenticationFilter {
 
         String role = claims.get("role", String.class);
 
-        ServerWebExchange modifiedExchange = exchange
+        ServerHttpRequest request = exchange
+                .getRequest()
                 .mutate()
-                .request(builder -> builder
-                        .header("X-User-Email", email)
-                        .header("X-User-Role", role)
-                )
+                .header("X-User-Email", email)
+                .header("X-User-Role", role)
                 .build();
 
-        return chain.filter(modifiedExchange);
-//        		exchange.getChain().filter(modifiedExchange);
+        return chain.filter(
+                exchange.mutate()
+                        .request(request)
+                        .build()
+        );
+    }
+
+    @Override
+    public int getOrder() {
+
+        return -1;
     }
 }
